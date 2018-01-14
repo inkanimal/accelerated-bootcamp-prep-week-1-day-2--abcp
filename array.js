72faff64c41bbcddf8c7b385c8667661ca514666
geoffrey woodcock <theatomproject@gmail.com>
2018-01-14 17:42:49 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,34 +1,40 @@
 function addElementToBeginningOfArray(array, element) {
- array[...array, element]// d the element to the beginning of the array without mutating the array
+ return [element,...array]// d the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  // add element to the beginning of the array by mutating the array
+  array.unshift(element)
+  return array// add element to the beginning of the array by mutating the array
 }
 
 function addElementToEndOfArray(array, element) {
-
+   return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+   array.push(element)
+   return array
 }
 
 function accessElementInArray(array, index) {
-
+   return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift(element)
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+   array.pop()
+   return array
 }
 
 function removeElementFromEndOfArray(array) {
-
+   return array.slice(0, array.length - 1)
 }
